1fa8a95d4b7b48ce08985cce1b7e950ab82122d7
simonelupone <simone.lupone@gmail.com>
2025-04-16 16:04:19 +0200

    feat: add base project with route to return a string

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello Boolean');
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
